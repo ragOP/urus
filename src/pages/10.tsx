eecf255908fa840047637cebff3384b5e9b0ca8b
ragOP <najmi.raghib@gmail.com>
2024-09-25 19:06:43 +0530

    dsdsds

--- src/pages/10.tsx
+++ src/pages/10.tsx
@@ -388,7 +388,7 @@ export default function Abc() {
           </div>
           <div className="spots-count">Spots remaining: 4</div>
           <div className="tap-direction">👇 TAP BELOW TO CALL 👇</div>
-          <a href="tel:+18446491108">             <div className="call-btn" onClick={handleCall}>             CALL (844) 649-1108             </div>           </a>
+          <a href="tel:+18666570134">             <div className="call-btn" onClick={handleCall}>            CALL (866) 657-0134         </div>           </a>
           <div className="sub-title">We Have Reserved Your Spot</div>
           <div className="sub-description">
             Due to high call volume, your official agent is waiting for only{" "}
